9defa31569d720e20e23e9b43044c1221cc903f0
You ki <91768634+Jmengying@users.noreply.github.com>
2023-12-20 15:41:04 +0800

    AliOssUtil.java

--- src/main/java/top/yjx1125/anime/utils/AliOssUtil.java
+++ src/main/java/top/yjx1125/anime/utils/AliOssUtil.java
@@ -12,14 +12,14 @@ import java.io.InputStream;
 
 public class AliOssUtil {
 
-    private static final String ENDPOINT = "https://oss-cn-hangzhou.aliyuncs.com";
+    private static final String ENDPOINT = "";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5tH17HqK8FRxWP6YhndE";
+    private static final String ACCESS_KEY_ID="";
 
-    private static final String ACCESS_KEY_SECRET="Zrdk9GO8O2OVxF0wSMmafM1vCIh9Xj";
+    private static final String ACCESS_KEY_SECRET="";
     // 填写Bucket名称，例如examplebucket。
-    private static final String BUCKET_NAME = "youki";
+    private static final String BUCKET_NAME = "";
         public static String uploadFile(String objectName, InputStream in) throws Exception {
 
             // 创建OSSClient实例。
